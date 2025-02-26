45d1cd117bf13011dd2e47357d864f25167d638a
Alejandro Garcia <65517825+alegarcia12@users.noreply.github.com>
2025-02-25 22:49:21 -0300

    test2

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        localStorage.clear();        
-        navigate("/login");
-    }, [navigate]);
+  useEffect(() => {
+    localStorage.clear();
+    navigate("/login");
+  }, [navigate]);
 
-    return null; //lei q sirve para no renderizar
+  return null; //lei q sirve para no renderizar
 };
 
 export default Logout;
